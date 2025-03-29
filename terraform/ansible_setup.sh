bdb557236397e5f74cec3f0ead681c42806cd5dd
olomg48 <olomg48@gmail.com>
2025-03-29 07:43:03 +0100

    a

--- terraform/ansible_setup.sh
+++ terraform/ansible_setup.sh
@@ -8,4 +8,4 @@ ansible-galaxy collection install google.cloud
 git clone https://github.com/olomg48/demo_ms.git /opt/demo
 chmod -R 777 /opt/demo
 
-#ansible-playbook -i inventory/hosts deploy.yaml -e "version=1.0 json_name=central-apex-450419-r2-b716b5e92620.json json_url=""
+#ansible-playbook -i inventory/hosts deploy.yaml -e "version=1.0 json_name=central-apex-450419-r2-b716b5e92620.json json_url="
